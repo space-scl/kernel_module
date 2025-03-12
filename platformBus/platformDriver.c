9af300f3970c47fd561f0d67493de4aa4d351ef8
chengson <chengson@amd.com>
2025-03-12 22:20:25 +0800

    Adding platform driver
    If the name member in platform_device matches the name member in platform_driver, kernel will connect the platform device and driver and the probe routine in the platform_driver will be executed.
    If they are mismatched, probe routine will not be executed.

---
+++ platformBus/platformDriver.c
@@ -0,0 +1,74 @@
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/array_size.h>
+#include <linux/device.h>
+#include <linux/mod_devicetable.h>
+
+static int testprobe (struct platform_device * pDev)
+{
+    printk("probe the device in platfrom driver\n");
+	return 0;
+}
+
+
+static void testremove (struct platform_device *pDev)
+{
+    printk("Remove the device in platform driver\n");
+	return;
+}
+
+
+/*
+static struct device_driver driver = {
+    .name = "myPlatformDevice",
+	.owner = THIS_MODULE,
+};
+*/
+
+const struct platform_device_id test_id_table = {
+	.name = "myPlatformDevice"
+	//.driver_data = 0,
+};
+
+struct platform_driver pDriver =  {
+	.probe = testprobe,
+    .remove = testremove,
+	.driver = {
+    	.name = "123",
+		.owner = THIS_MODULE,
+    },
+	.id_table = &test_id_table
+};
+
+static int platform_driver_init(void)
+{
+	int ret;
+
+	ret = platform_driver_register(&pDriver);
+	if (ret != 0) {
+	    printk("Fail to register platform driver\n");
+		return -1;
+	}
+	printk("register platform driver\n");
+	return 0;
+
+}
+
+static void platform_driver_exit(void)
+{
+	platform_driver_unregister(&pDriver);
+	return;
+}
+
+
+
+module_init(platform_driver_init);
+module_exit(platform_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("A simple platform bus driver");
+
+
+
+
